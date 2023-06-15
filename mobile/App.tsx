8432799e8a1b59c6ea6869e43bb36045a3452f82
Marcos Oliveira <marcosluz312@hotmail.com>
2023-06-15 03:13:17 -0300

    init

---
+++ mobile/App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+export default function App() {
+  return (
+    <View className='bg-slate-600 flex-1 justify-center align-middle items-center'>
+      <Text className='text-white'>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )};
+
